5878897edbcc3aec5efee76d0efd2db90c04e571
Jorge Oliveira <jorgedroliveira10@gmail.com>
2023-07-12 22:03:32 +0100

    Fixed constant name

--- f1-data-api/index.js
+++ f1-data-api/index.js
@@ -4,12 +4,12 @@ const port = 3000;
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger.json');
 
-const circuit = require('./src/api/circuits');
+const circuits = require('./src/api/circuits');
 
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-app.use('/api/f1', circuit);
+app.use('/api/f1', circuits);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
