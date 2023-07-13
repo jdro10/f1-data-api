5d7194b229a3f6954b94fc3e99abe6a39af70274
Jorge Oliveira <jorgedroliveira10@gmail.com>
2023-07-13 21:03:31 +0100

    Implemented new GET endpoint that returns a list of seasons where a specific constructor participated

--- f1-data-api/src/api/seasons.js
+++ f1-data-api/src/api/seasons.js
@@ -36,4 +36,24 @@ router.get("/circuits/:circuitName/seasons", async (req, res) => {
   return res.json(seasons);
 });
 
+router.get("/constructors/:constructorName/seasons", async (req, res) => {
+  const queryString = `SELECT DISTINCT s.*
+                      FROM seasons s
+                      JOIN races r ON r.year = s.year
+                      JOIN circuits c ON c.circuitId = r.circuitId
+                      JOIN constructorResults cr ON cr.raceId = r.raceId
+                      JOIN constructors ct ON ct.constructorId = cr.constructorId
+                      WHERE ct.constructorRef = ?
+                      ORDER BY s.year
+                      LIMIT ?`;
+
+  const queryLimit = req.query.limit !== undefined ? parseInt(req.query.limit) : DEFAULT_QUERY_LIMIT;
+
+  const seasons = await db.query(queryString, [req.params.constructorName, queryLimit]).catch((err) => {
+    throw err;
+  });
+
+  return res.json(seasons);
+});
+
 module.exports = router;
